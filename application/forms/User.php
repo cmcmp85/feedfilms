9d106407fcf6a89d963825bda043bededf9b06a6
Carmen <cmcmp85@gmail.com>
2013-11-26 11:09:34 +0100

    register empezando

--- application/forms/User.php
+++ application/forms/User.php
@@ -79,6 +79,8 @@ class Application_Form_User extends Zend_Form
         ->addFilter('StripTags')
         ->addFilter('StringTrim')
         ->addValidator('StringLength',false,array(3,200))
+        ->addValidator('regex', false, array('/^[0-9]{8}[A-Z]$/i'))
+        ->addValidator('Db_NorecordExists',true,array('table'=>'users','field'=>'nid'))
         ->setAttrib('size', 30)
         ->setAttrib('maxlength', 255)
         ->setOptions(array('class'=>'form-control'))
@@ -107,7 +109,11 @@ class Application_Form_User extends Zend_Form
         ->addValidator('NotEmpty', true)
         ->setmultiOptions(array('1'=>'Activo', '0'=>'Inactivo'))
         ->setAttrib('maxlength', 200)
-        ->setAttrib('size', 1);
+        ->setAttrib('size', 1)
+        ->setOptions(array('class'=>'form-control'))
+        ->setDecorators(array(array('ViewScript', array(
+	        		'viewScript' => 'forms/_element_checkbox.phtml'
+	        ))));
         
         $idusertype = new Zend_Form_Element_Select('idusertype');
         $idusertype->setLabel('User Type')
@@ -115,15 +121,74 @@ class Application_Form_User extends Zend_Form
         ->addValidator('NotEmpty', true)
         ->setmultiOptions($this->_selectOptions())
         ->setAttrib('maxlength', 200)
-        ->setAttrib('size', 1);
+        ->setAttrib('size', 1)
+        ->setOptions(array('class'=>'form-control'))
+	->setDecorators(array(array('ViewScript', array(
+	        		'viewScript' => 'forms/_element_text.phtml'
+	        ))));
 
+        $technology = new Zend_Form_Element_MultiCheckbox('technology');
+        $technology->setLabel('Tecnologies:')
+        ->setRequired(true)
+        ->setValue('php')
+        ->addValidator('NotEmpty', true, array('messages'=>array(Zend_Validate_NotEmpty::IS_EMPTY=>'Valor requerido')))
+        ->setmultiOptions(array('php'=>'Php', 'java'=>'Java', 'mysql'=>'MySQL'))
+        ->setOptions(array('class'=>'form-control'))
+        ->setDecorators(array(array('ViewScript', array(
+	        		'viewScript' => 'forms/_element_category_multi_checkbox.phtml'
+	        ))));
+        
+        $gender = new Zend_Form_Element_Radio('gender');
+        $gender->setLabel('Género')
+        ->setValue('1')
+        ->setMultiOptions(array('1'=>'Femenino', '0'=>'Masculino'))
+        ->setRequired(true)
+        ->addValidator('NotEmpty', true)
+        ->setOptions(array('class'=>'form-control'))
+	->setDecorators(array(array('ViewScript', array(
+                            'viewScript' => 'forms/_element_radio.phtml'
+	        ))));
+        
+        $languages = new Zend_Form_Element_Multiselect('languages');
+        $languages->setLabel('Languages')
+        ->setValue('1')
+        ->setMultiOptions(array('0'=>'Español', '1'=>'English','2'=>'Français','3'=>'Deutsch'))
+        ->setRequired(true)
+        ->addValidator('NotEmpty', true)
+        ->setOptions(array('class'=>'form-control'));
+        
+        $city = new Zend_Form_Element_Select('cities');
+        $city->setLabel('City')
+        ->setRequired(true)
+        ->addValidator('NotEmpty', true)
+        ->setmultiOptions(array('0'=>'Madrid', '1'=>'Barcelona','3'=>'Zaragoza'))
+        ->setAttrib('maxlength', 200)
+        ->setAttrib('size', 1)
+        ;
+        
+		$destination=realpath($_SERVER['DOCUMENT_ROOT']);
+        $image = new Zend_Form_Element_File('image');
+        $image->setLabel('Image (512kb size, jpg,png,gif)')
+        ->addValidator('NotEmpty', true)
+        ->setDestination($destination)
+        ->addFilter('StripTags')
+        ->addFilter('StringTrim')
+        ->addValidator('NotExists', false, array($destination))
+        ->addValidator('Count', false, array(1))
+        ->addValidator('Extension', true, array('jpg,png,gif'))
+        ->addValidator('Size', false, array(512000));        
+        
+    
+        
         $submit = new Zend_Form_Element_Submit('submit');
 		$submit->setAttrib('id', 'submitbutton');
 		
 		$this->addElements(array($id, 
-								$email, $password, $display_name, $state,
-								$description,$nid,$url,
-								$idusertype, $submit));
+                                        $email, $password, $display_name, $state,
+                                        $description,$nid,$url,
+                                        $idusertype, $technology, $gender,
+                                        $languages, $city,
+                                        $submit));
     }
     
     protected function _selectOptions()
